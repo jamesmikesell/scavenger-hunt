12cda7f3b5d0104f14854501b468feb7c65de57f
James Mikesell <james.mikesell@gmail.com>
2022-07-04 10:12:59 -0400

    adding version info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scavenger-hunt';
+
+  getVersion(): string {
+    return environment.version;
+  }
+
 }
